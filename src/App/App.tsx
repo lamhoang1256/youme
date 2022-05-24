6d5a21b752c57607c3230f70141119f86f983617
lamhoang1256 <lamhoang1256@gmail.com>
2022-05-24 17:41:40 +0700

    Chore: Remove Community page because not useful

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -8,7 +8,6 @@ const Detail = React.lazy(() => import("pages/Detail/Detail"));
 const Watch = React.lazy(() => import("pages/Watch/Watch"));
 const Explore = React.lazy(() => import("pages/Explore/Explore"));
 const Category = React.lazy(() => import("pages/Category/Category"));
-const Community = React.lazy(() => import("pages/Community/Community"));
 
 const App = () => {
   return (
@@ -21,7 +20,6 @@ const App = () => {
             <Route path="/watch/:id" element={<Watch />} />
             <Route path="/category/:id" element={<Category />} />
             <Route path="/explore" element={<Explore />} />
-            <Route path="/community" element={<Community />} />
           </Route>
           <Route path="/login" element={<Login />} />
         </Routes>
