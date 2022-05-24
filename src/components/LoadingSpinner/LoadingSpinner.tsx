f474513b74ba04d05fe516442422b29aa9663704
lamhoang1256 <lamhoang1256@gmail.com>
2022-05-24 21:21:47 +0700

    Style: custom loading spinner css

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -4,14 +4,21 @@ const StyledLoadingSpinner = styled.div`
   margin: 40px auto;
   width: 40px;
   height: 40px;
+  pointer-events: none;
+  border: 4px solid transparent;
+  border-color: #eee;
+  border-top-color: var(--secondary-color);
+  border-radius: 50%;
+  animation: loadingspin 1.2s linear infinite;
+  @keyframes loadingspin {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 `;
 
 const LoadingSpinner = () => {
-  return (
-    <StyledLoadingSpinner>
-      <img src={`${process.env.REACT_APP_PUBLIC}/images/spinner-no-bg.gif`} alt="Spinner" />
-    </StyledLoadingSpinner>
-  );
+  return <StyledLoadingSpinner />;
 };
 
 export default LoadingSpinner;
