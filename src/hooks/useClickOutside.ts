7848db33e0e036601254dd44b7369f09776a225d
lamhoang1256 <lamhoang1256@gmail.com>
2022-05-25 15:28:37 +0700

    Chore: custom hook useClickOutside for searchBar

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,21 @@
+import { RefObject, useEffect } from "react";
+
+export const useOnClickOutside = <T extends HTMLElement = HTMLElement>(
+  ref: RefObject<T>,
+  handler: (event: Event) => void,
+) => {
+  useEffect(() => {
+    const listener = (event: any) => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      handler(event);
+    };
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, handler]);
+};
