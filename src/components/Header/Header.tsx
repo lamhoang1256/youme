045cd43c664d5f7f51c59360c96143c90f678ec6
lamhoang1256 <lamhoang1256@gmail.com>
2022-05-25 22:07:50 +0700

    Style: responsive header search at mobile and tablet

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,15 @@
 import IonIcon from "@reacticons/ionicons";
-import { useMediaQuery } from "hooks/useMediaQuery";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useMediaQuery } from "hooks/useMediaQuery";
 import { StyledHeader } from "./header.style";
 import HeaderMenu from "./module/HeaderMenu/HeaderMenu";
 import HeaderSearch from "./module/HeaderSearch/HeaderSearch";
 
 const Header = () => {
-  const isMobile = useMediaQuery("(max-width:767.98px)");
+  const isTablet = useMediaQuery("(max-width:1023.98px)");
   const [showMenu, setShowMenu] = useState(false);
-  const [showSearchMobile, setShowSearchMobile] = useState(!isMobile);
+  const [showSearchMobile, setShowSearchMobile] = useState(!isTablet);
 
   const handleToggleMenu = () => {
     setShowMenu(!showMenu);
@@ -17,6 +17,10 @@ const Header = () => {
   const handleToggleSearch = () => {
     setShowSearchMobile(!showSearchMobile);
   };
+
+  useEffect(() => {
+    setShowSearchMobile(!isTablet);
+  }, [isTablet]);
 
   return (
     <StyledHeader>
@@ -27,16 +31,13 @@ const Header = () => {
           </Link>
           <HeaderMenu showMenu={showMenu} handleToggleMenu={handleToggleMenu} />
           {showSearchMobile && <HeaderSearch />}
-          <button className="header-search primary" type="button" onClick={handleToggleSearch}>
-            <IonIcon name="search-outline" />
+          <button type="button" className="header-auth primary">
+            Sign In
           </button>
-          <div className="header-action">
-            <button type="button">Sign Up</button>
-            <button type="button" className="primary">
-              Sign In
-            </button>
-          </div>
           <div className="header-mobile">
+            <button className="header-search primary" type="button" onClick={handleToggleSearch}>
+              <IonIcon name="search-outline" />
+            </button>
             <button type="button" onClick={handleToggleMenu} className="header-open">
               <IonIcon name="menu-outline" />
             </button>
