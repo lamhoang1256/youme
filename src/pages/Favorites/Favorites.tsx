fa9760691c7c382d6d6407f5298b41a6fac95b95
lamhoang1256 <lamhoang1256@gmail.com>
2022-05-28 22:08:56 +0700

    Fix: add movie favorite many times if not reload page

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,15 +1,36 @@
 import { useAppSelector } from "App/store";
 import Breadcrumb from "components/Breadcrumb/Breadcrumb";
 import MovieList from "components/MovieList/MovieList";
+import { db } from "firebase-app/firebase-config";
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+
+const crumbs = [
+  { id: 1, label: "Home", path: "/" },
+  { id: 2, label: "Favorites", path: "/favorites" },
+];
+
+interface IFavorites {
+  coverVerticalUrl: string;
+  domainType: number;
+  id: string;
+  name: string;
+}
 
 const Favorites = () => {
-  const crumbs = [
-    { id: 1, label: "Home", path: "/" },
-    { id: 2, label: "Favorites", path: "/favorites" },
-  ];
-
   const { currentUser } = useAppSelector((state) => state.auth);
-  const favorites = currentUser?.favorites;
+  const [favorites, setFavorites] = useState<IFavorites[]>([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      if (currentUser?.uid) {
+        const colRef = doc(db, "users", currentUser?.uid);
+        const data = await getDoc(colRef);
+        setFavorites(data.data()?.favorites);
+      }
+    };
+    getData();
+  }, [currentUser]);
 
   return (
     <div className="container">
