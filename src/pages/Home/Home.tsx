5fc6f8b31581af3cd56fa52b523290c42ed5522d
lamhoang1256 <lamhoang1256@gmail.com>
2022-05-31 09:01:14 +0700

    Chore: use infinite at Home but not use uuid MovieList

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,12 @@
-import { useEffect, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
-import { IHomeSection } from "interfaces/home";
-import { getHome } from "apis/configAPI";
-import SkeletonTitle from "components/Skeleton/SkeletonTitle";
 import { Link } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+import useSWRInfinite from "swr/infinite";
+import { getHome } from "apis/configAPI";
+import InfiniteScroll from "react-infinite-scroll-component";
+import LoadingSpinner from "components/LoadingSpinner/LoadingSpinner";
+import SkeletonTitle from "components/Skeleton/SkeletonTitle";
+import { useEffect, useState } from "react";
+import { IHomeSection } from "interfaces/home";
 import HomeBanner from "./module/HomeBanner/HomeBanner";
 import HomePopular from "./module/HomePopular/HomePopular";
 import HomeList from "./module/HomeList/HomeList";
@@ -13,26 +16,26 @@ import { StyledHome, StyledWrapperLayout } from "./home.style";
 import { StyledHomeList } from "./module/HomeList/homeList.style";
 
 const Home = () => {
-  const [loadingSections, setLoadingSections] = useState<boolean>(true);
   const [homeSections, setHomeSections] = useState<IHomeSection[]>([]);
-
-  const fetchHomeSections = async () => {
-    setLoadingSections(true);
-    try {
-      const { data } = await getHome({ page: 0 });
-      const sectionMovies = data.recommendItems.filter(
-        (section: any) => section.bannerProportion !== 1 && section.coverType === 1,
-      );
-      setHomeSections(sectionMovies);
-      setLoadingSections(false);
-    } catch (error) {
-      setLoadingSections(false);
-    }
-  };
+  const getKey = (index: number) => `page-${index || 1}`;
+  const { data, error, setSize } = useSWRInfinite(
+    getKey,
+    (key: string) => getHome({ page: Number(key.split("page-")[1]) }),
+    {
+      revalidateFirstPage: false,
+    },
+  );
 
   useEffect(() => {
-    fetchHomeSections();
-  }, []);
+    if (!data) return;
+    const sectionMovies = data?.reduce((prevData: any, currentData: any) => {
+      const suitableSections = [...currentData.data.recommendItems]?.filter(
+        (section: any) => section.bannerProportion !== 1 && section.coverType === 1,
+      );
+      return [...prevData, ...suitableSections];
+    }, []);
+    setHomeSections(sectionMovies);
+  }, [data]);
 
   return (
     <StyledHome>
@@ -40,7 +43,7 @@ const Home = () => {
       <StyledWrapperLayout className="container">
         <div className="wrapper-main">
           <HomePopular />
-          {loadingSections && (
+          {!data && (
             <StyledHomeList>
               <SkeletonTitle />
               <div className="home-list">
@@ -52,18 +55,28 @@ const Home = () => {
               </div>
             </StyledHomeList>
           )}
-
-          {!loadingSections && (
-            <>
-              {homeSections.map((homeSection) => (
-                <HomeList key={homeSection.homeSectionId} homeSection={homeSection} />
-              ))}
-              <Link to="/explore">
-                <button type="button" className="seemore">
-                  See more
-                </button>
-              </Link>
-            </>
+          {data && (
+            <InfiniteScroll
+              dataLength={data?.length || 0}
+              next={() => setSize((size) => size + 1)}
+              hasMore={!error && data?.slice(-1)?.[0]?.data?.recommendItems?.length !== 0}
+              loader={<LoadingSpinner />}
+              endMessage={
+                <Link to="/explore">
+                  <button type="button" className="seemore">
+                    See more
+                  </button>
+                </Link>
+              }
+            >
+              {data && (
+                <>
+                  {homeSections.map((homeSection) => (
+                    <HomeList key={homeSection.homeSectionId} homeSection={homeSection} />
+                  ))}
+                </>
+              )}
+            </InfiniteScroll>
           )}
         </div>
         <div className="wrapper-side">
@@ -75,15 +88,3 @@ const Home = () => {
 };
 
 export default Home;
-
-// category: 1
-// contentType: "DRAMA"
-// id: 15870
-// imageUrl: "https://img.netpop.app/cover/20220415/1649993224652_1a1324cdda65685844de7ea9ee348a70ofU2TlwDOOuWtQ456jeGwRNkyp2.jpg"
-// jumpAddress: "tiktik://jump/detail?id=15870&type=1"
-// jumpType: ""
-// needLogin: false
-// resourceNum: 6
-// resourceStatus: 2
-// showMark: true
-// title: "The Sound of Magic"
