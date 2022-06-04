e22ac313d5b816f7db1d34d84fa520596558bbf4
lamhoang1256 <lamhoang1256@gmail.com>
2022-06-04 22:10:58 +0700

    Chore: rename HeaderUser to HeaderDropdown

--- src/layouts/MainLayout/module/Header/module/HeaderDropdown/headerDropdown.style.ts
+++ src/layouts/MainLayout/module/Header/module/HeaderDropdown/headerDropdown.style.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const StyledHeaderUser = styled.div`
+export const StyledHeaderDropdown = styled.div`
   z-index: 100;
   .header-avatar {
     width: 45px;
