145539682544c390403b12b74c4389d590ee8b91
lamhoang1256 <lamhoang1256@gmail.com>
2022-06-05 11:44:28 +0700

    Feat: add Header User

---
+++ src/layouts/MainLayout/module/Header/module/HeaderUser/HeaderUser.tsx
@@ -0,0 +1,51 @@
+import IonIcon from "@reacticons/ionicons";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import { auth } from "firebase-app/firebase-config";
+import { signOut } from "firebase/auth";
+import { useAppSelector } from "App/store";
+import ButtonGradient from "components/Button/ButtonGradient";
+import { PUBLIC_IMAGE } from "constants/path";
+import { StyledHeaderUser } from "./headerUser.style";
+
+const HeaderUser = () => {
+  const { currentUser } = useAppSelector((state) => state.auth);
+  const { t } = useTranslation();
+  const avatar = currentUser?.avatar ? currentUser.avatar : `${PUBLIC_IMAGE}/header-avatar.webp`;
+  const username = currentUser?.username ? currentUser?.username : "Guest";
+
+  const handleLogout = () => {
+    signOut(auth);
+  };
+
+  return (
+    <StyledHeaderUser>
+      <img src={avatar} alt="avatar" className="user-avatar" />
+      <div className="user">
+        <div className="user-header">
+          <img src={avatar} alt="avatar" className="user-avatar" />
+          <div className="user-info">
+            <span className="user-username">{username}</span>
+            <span>{t("User")}</span>
+          </div>
+        </div>
+        <ul className="user-list">
+          <li className="user-item">
+            {currentUser ? (
+              <ButtonGradient className="header-auth secondary" onClick={handleLogout}>
+                <IonIcon name="log-out-outline" />
+                <span>{t("Logout")}</span>
+              </ButtonGradient>
+            ) : (
+              <Link to="/sign-in">
+                <ButtonGradient className="header-auth primary">{t("Sign In")}</ButtonGradient>
+              </Link>
+            )}
+          </li>
+        </ul>
+      </div>
+    </StyledHeaderUser>
+  );
+};
+
+export default HeaderUser;
